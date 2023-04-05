821fcd99990fecc1507f73b900698dd4719b9780
Prateek <129204458+prateek-who@users.noreply.github.com>
2023-04-05 20:10:20 +0530

    Update new_proj_try.java

--- new_proj/new_proj_try.java
+++ new_proj/new_proj_try.java
@@ -12,17 +12,19 @@ public class new_proj_try
 
 class print_on_screen
 {
+    trader trader_obj1 = new trader();
     Scanner sc= new Scanner(System.in);
-    String[][] ar= new String[7][25];
+    String[][] ar= new String[7][25]; //change row and colums here first
     String sp=" ";
     int row,column;
     int r_ch=5,c_ch=7;
     public void prnt(){
         for(row=0;row<7;row++){
-            for(column=0;column<25;column++) {
+            for(column=0;column<25;column++) {   //Don't forget to change rows and columns here aswell
                 if (row == 0 || row == 6) {
                     ar[row][column]="_";
                 }
+                //If anything has to be printed on the screen, make sure to add it in (else if)
                 else if(column==0 || column==24){
                     ar[row][column]="|";
                 }
@@ -33,7 +35,7 @@ class print_on_screen
 
                 else if(row== 3 && column== 18){
                     ar[row][column]="?";
-                }
+                } // Trader coordiantes. Can try to make it random each time.....?
                 else{
                     ar[row][column]=sp;
                 }
@@ -44,30 +46,64 @@ class print_on_screen
     }
 }
 
-class trader extends print_on_screen
+class trader
 {
-    int choice;
-    public void interact(){
-        System.out.println("(?): What are you still doing here?\n1.Interact\n2.Leave");
-        choice=sc.nextInt();
+    Scanner sc= new Scanner(System.in);
+    //Can add more dialogues and even more characters in the future
+    int choice,count=1;
 
-        if(choice==1){
-            System.out.println("(*): I was just passing by....Who are you?");
-            System.out.println("'The mysterious person stares at you'\n(?):Nothing is left here kid....It's all over..*sigh*");
-            System.out.println("(?): Do you have some water?\n1.Yes\n2.No");
+    public void interact(){
+        if(count==1) {
+            System.out.println("(?): What are you still doing here?\n1.Interact\n2.Leave");
+            choice = sc.nextInt();
+
+            if (choice == 1) {
+                System.out.println("(*): I was just passing by....Who are you?");
+                System.out.println("*The mysterious person stares at you*\n(?):Nothing is left here kid....It's all over..*He sighs..*");
+                System.out.println("(?): Do you have some water?\n1.Yes\n2.No");
+                choice = sc.nextInt();
+                if (choice == 1) {
+                    System.out.println("(?): Thanks traveller...I will now have to leave..\n*The mysterious character dissappears into his broken shed*");
+                    System.out.println("*You decide to leave*");
+                }
+                else if (choice == 2) {
+                    System.out.println("(?): No problem traveller...Safe passage ahead..");
+                    System.out.println("*You decide to leave*");
+                }
+                else{
+                    System.out.println("*The mysterious figure looks at you weirdly and leaves*");
+                    System.out.println("*You decide to leave*");
+                }
+            }
+            else if(choice == 2){
+                System.out.println("*You decide to leave*");
+            }
+            else{
+                System.out.println("*The mysterious figure looks at you weirdly and leaves*");
+                System.out.println("*You decide to leave*");
+            }
+            count++;
+        }
+
+        //if characeters comes here again, this will be played!
+        else if(count==2){
+            System.out.println("*An empty broken shed...You wonder where the mysterious figure went..\n1. Look inside\n2. Leave");
             choice=sc.nextInt();
             if(choice==1){
-                System.out.println("Thanks traveller...I will now have to leave..\nThe mysterious character dissappears");
+                System.out.println("*You peep inside the shed.There is a mattress stuffed in a corner. A broken lamp rests nexts to it. Nothing of interest seems to be here*");
+                System.out.println("*You decide to leave*");
             }
-            else if(choice==2){
-                System.out.println("No problem traveller...Safe passage ahead..");
+            else{
+                System.out.println("*You decide to leave*");
             }
+        }
+        else{
+            System.out.println("Sorry, I did");
         }
     }
 }
 class act_ply extends print_on_screen
 {
-    trader trader_obj1 = new trader();
     String movement;
     public void moving(){
         System.out.println("You are '*'");
@@ -101,7 +137,6 @@ class act_ply extends print_on_screen
                 else if(c_ch==0){
                     c_ch++;
                 }
-
                 else if(c_ch==24){
                     c_ch--;
                 }
